299889fe61135a8e9f8d3501bbfe20c12038df38
yasp-g <jasper.h.g@icloud.com>
2023-08-10 12:02:21 +0200

    - removed TODO for deleting utils.context (closed) - added `logging_check` bool var   - not being used yet   - idea is allowing users to turn off chat logging in future - added `theme="sudeepshouche/minimalist"` theme   - looks better - major layout changes across both tabs   - boxed `chat` with a row containing `msg_box` and `submit_btn`     - this groups these components together, which enforces that they are part of the same system     - drawback of this is `clear_btn` is now huge and expands across the entire width of the page       - this looks kinda bad on desktop   - put `file_types` and `save_btn` in a column and then boxed it     - added css for centering these components horizontally and vertically     - added a markdown label at the top of the column because centering horizontally messed with `file_types` built in label   - put `file_out` in the same row as this column     - this looked better because proportions were off in desktop when this and `file_types` + `save_btn` were in their own rows   - needed to add `min_width` to the `gr.Column()` and `file_out` because `file_out` was squeezing the other components without wrapping to the next row on mobile - removed `reset_msg_box_args` and its associated runs with `user_submit` and `submit_click`   - wasn't needed anymore. see commit with chatbot.py - changed `save_btn` to be uninteractive unless a selection is made in `file_types`   - wasn't really that necessary (wasn't crashing the app) but makes the user experience more intuitive

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ from datetime import datetime
 from utils import s3, chatbot
 from utils.config import STATUS_MSGS, SYSTEM_PROMPT, TITLE, SUB_TITLE, DESCRIPTION_TOP
 
-# TODO: delete utils.context (moved it into config.py)
-
 chat_saving = True
+logging_check = True
 print("OPENAI_API_KEY:", bool(os.environ.get("OPENAI_API_KEY")))
 
 # Logging
@@ -27,7 +26,7 @@ with open("static/custom.css", "r", encoding="utf-8") as f:
     customCSS = f.read()
 
 # Gradio App
-with gr.Blocks(css=customCSS) as demo:
+with gr.Blocks(css=customCSS, theme="sudeepshouche/minimalist") as demo:
     # SETUP
     # Session variables
     session_chat = gr.State([])
@@ -44,26 +43,24 @@ with gr.Blocks(css=customCSS) as demo:
 
     # Chatbot Tab
     with gr.Tab("Chatbot"):
-        with gr.Row():
+        with gr.Box():
             chat = gr.Chatbot(label="Chat")
-        with gr.Row(equal_height=True):
-            with gr.Column(scale=25):
-                msg_box = gr.Textbox(show_label=False, placeholder="Type a message.", container=False)
-            with gr.Column(scale=0):
-                submit_btn = gr.Button("Send")
-        with gr.Row():
-            clear_btn = gr.Button("Clear Conversation")
+            with gr.Row(variant='panel', equal_height=True):
+                msg_box = gr.Textbox(show_label=False, placeholder="Type a message.", container=False, scale=5)
+                submit_btn = gr.Button("Send", scale=1)
+        clear_btn = gr.Button("Clear Conversation")
+        # chat_saving_check = gr.Checkbox(True, label="Chat Logging", visible=logging_check, interactive=True)
 
     # Download Tab
     if chat_saving:
         with gr.Tab("Download Chat"):
-            with gr.Column():
-                with gr.Row():
-                    file_types = gr.CheckboxGroup([".txt", ".json", ".csv"],
-                                                  label="Select file type(s) to download")
-                    save_btn = gr.Button("Save Chat")
-                with gr.Row():
-                    file_out = gr.File(label="File(s)")
+            with gr.Row():
+                with gr.Box(elem_id="download_files_box"):
+                    with gr.Column(scale=1, min_width=480):
+                        gr.Markdown("<center>Select file type(s) and click Save Chat</center>")
+                        file_types = gr.CheckboxGroup([".txt", ".json", ".csv"], show_label=False, elem_id="file_types")
+                        save_btn = gr.Button("Save Chat", scale=1, interactive=False, elem_id="save_files_inputs")
+                file_out = gr.File(label="Saved File(s)", scale=2, min_width=480, interactive=False)
 
     server_name = gr.Markdown(f"Server: {os.getenv('INSTANCE_NAME')}", elem_id="server_name")
 
@@ -84,32 +81,15 @@ with gr.Blocks(css=customCSS) as demo:
         queue=True
     )
 
-    reset_msg_box_args = dict(
-        fn=lambda: gr.update(interactive=True),
-        inputs=None,
-        outputs=msg_box,
-        queue=False
-    )
-
-    # Temporary
-    # TODO: Remove when not needed
-    # def print_chat(s_history, s_context):
-    #     print(f"history: {s_history}")
-    #     print(f"context: {s_context[1:]}")
-
     # APP ACTIONS
     # Textbox enter
     user_submit = msg_box.submit(**user_msg_args)
     user_submit.then(**assistant_msg_args)
-    user_submit.then(**reset_msg_box_args)
-    # user_submit.then(print_chat, [session_chat, session_context])
     user_submit.then(fn=s3.upload, inputs=[session_id, session_time, session_context])
 
     # Textbox + Send button
     submit_click = submit_btn.click(**user_msg_args)
     submit_click.then(**assistant_msg_args)
-    submit_click.then(**reset_msg_box_args)
-    # submit_click.then(print_chat, [session_chat, session_context])
     submit_click.then(fn=s3.upload, inputs=[session_id, session_time, session_context])
 
     # Clear button
@@ -122,11 +102,14 @@ with gr.Blocks(css=customCSS) as demo:
 
     # Save Chat button
     if chat_saving:
-        save_click = save_btn.click(fn=s3.serve_files, inputs=[session_id, session_time, file_types], outputs=file_out)
+        file_types.input(fn=lambda types: gr.update(interactive=True) if any(types) else gr.update(interactive=False),
+                         inputs=[file_types],
+                         outputs=[save_btn])
+        save_btn.click(fn=s3.serve_files, inputs=[session_id, session_time, file_types], outputs=file_out)
 
 logger.info('Launching the app...')
 try:
-    # demo.queue().launch() # for local
-    demo.queue().launch(server_name="0.0.0.0", server_port=8080, ssl_verify=False)
+    demo.queue().launch(share=True)  # for local
+    # demo.queue().launch(server_name="0.0.0.0", server_port=8080, ssl_verify=False) # for aws
 except Exception as e:
     logger.error('An error occurred while launching the app:', exc_info=True)
