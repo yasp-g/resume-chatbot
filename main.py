56a5fb8be81ab9bf43f5b286ec82d10477f7dd76
yasp-g <jasper.h.g@icloud.com>
2023-11-14 19:47:04 +0100

    `main.py` - moved logging set up to top - created `# SETUP` section that initializes the s3 bucket path and api key - added `serve_error()` function and a `session_error` `gr.State()` object   - `serve_error()` is run as the last step of `user_submit` or `submit_click` to serve `session_error` as a `gr.Warning()` - fixed issue where `submit_btn` was expanding vertically if multiple lines of text were typed `msg_box`.   - setting `equal_height=False` and `elem_id='submit_button_container'` (new CSS element) in the `gr.Row` fixed this - added max lines limit to `msg_box` - added `variant` parameter to all the buttons - set `save_btn` to default to `interactive=False`   - selecting any of the `file_types` now makes `save_btn` interactive - added `submit_btn` and `msg_box` to `outputs` of `user_msg_args` and `assistant_msg_args`   - `user_msg_args` makes `submit_btn` and `msg_box` not interactive when a message is submitted   - `assistant_msg_args` makes `submit_btn` and `msg_box` interactive again when the response is generating - added `session_error` to `assistant_msg_args` - added many more steps to `clear_click` for additional things that also needed to be reset - added `concurrency_count=1` to `demo.queue()` - added exception handling to launching of the app

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-import uuid
 import logging
 import os
 import sys
+import uuid
 
 import gradio as gr
 
 from datetime import datetime
-from utils import s3, chatbot
+from utils import aws, chatbot
 from utils.config import STATUS_MSGS, SYSTEM_PROMPT, TITLE, SUB_TITLE, DESCRIPTION_TOP
 
-chat_saving = True
-logging_check = True
-print("OPENAI_API_KEY:", bool(os.environ.get("OPENAI_API_KEY")))
-
-# Logging
+# LOGGING
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stdout)
@@ -21,11 +17,30 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+# SETUP
+chat_saving = True
+# logging_check = True
+aws.get_bucket_path()
+if not os.environ.get('OPENAI_API_KEY'):
+    aws.get_api_key()
+
+logger.info(f"INSTANCE_NAME: {os.environ.get('INSTANCE_NAME')}")
+logger.info(f"ENVIRONMENT: {os.environ.get('ENVIRONMENT')}")
+logger.info(f"OPENAI_API_KEY check: {bool(os.environ.get('OPENAI_API_KEY'))}")
+logger.info(f"S3_BUCKET_NAME check: {bool(os.environ.get('S3_BUCKET_NAME'))}")
+
+
+def serve_error(error):
+    # print("error warning", error)
+    if error:
+        return gr.Warning(f"{error}")
+
+
 # CSS
 with open("static/custom.css", "r", encoding="utf-8") as f:
     customCSS = f.read()
 
-# Gradio App
+# GRADIO APP
 with gr.Blocks(css=customCSS, theme="sudeepshouche/minimalist") as demo:
     # SETUP
     # Session variables
@@ -33,6 +48,7 @@ with gr.Blocks(css=customCSS, theme="sudeepshouche/minimalist") as demo:
     session_context = gr.State([{'role': 'system', 'content': SYSTEM_PROMPT}])
     session_id = gr.State(lambda: str(uuid.uuid1()))
     session_time = gr.State(lambda: datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+    session_error = gr.State()
 
     # GRADIO LAYOUT
     # Title, description, status display
@@ -45,39 +61,56 @@ with gr.Blocks(css=customCSS, theme="sudeepshouche/minimalist") as demo:
     with gr.Tab("Chatbot"):
         with gr.Box():
             chat = gr.Chatbot(label="Chat")
-            with gr.Row(variant='panel', equal_height=True):
-                msg_box = gr.Textbox(show_label=False, placeholder="Type a message.", container=False, scale=5)
-                submit_btn = gr.Button("Send", scale=1)
-        clear_btn = gr.Button("Clear Conversation")
+            with gr.Row(variant='panel', equal_height=False, elem_id='submit_button_container'):
+                # TODO: check if container=False bug has been addressed
+                msg_box = gr.Textbox(show_label=False, placeholder="Type a message.", scale=5, max_lines=10)
+                submit_btn = gr.Button("Send", scale=1, variant='primary')
+        clear_btn = gr.Button("Clear Conversation", variant='stop')
+        # error_info_btn = gr.Button("Error Info")
         # chat_saving_check = gr.Checkbox(True, label="Chat Logging", visible=logging_check, interactive=True)
 
     # Download Tab
     if chat_saving:
         with gr.Tab("Download Chat"):
             with gr.Row():
-                with gr.Box(elem_id="download_files_box"):
+                with gr.Box(elem_id="download_files_container"):
                     with gr.Column(scale=1, min_width=480):
                         gr.Markdown("<center>Select file type(s) and click Save Chat</center>")
                         file_types = gr.CheckboxGroup([".txt", ".json", ".csv"], show_label=False, elem_id="file_types")
-                        save_btn = gr.Button("Save Chat", scale=1, interactive=False, elem_id="save_files_inputs")
+                        save_btn = gr.Button("Save Chat", scale=1, interactive=False, variant='primary',
+                                             elem_id="save_files_inputs")
                 file_out = gr.File(label="Saved File(s)", scale=2, min_width=480, interactive=False)
 
     server_name = gr.Markdown(f"Server: {os.getenv('INSTANCE_NAME')}", elem_id="server_name")
 
-    # Chatbot arg simplification
+    # CHATBOT ACTION ARGUMENTS
     user_msg_args = dict(
         fn=chatbot.user_msg,
-        inputs=[msg_box, session_chat, session_context],
-        outputs=[msg_box, session_chat, chat, session_context, status_display],
+        inputs=[msg_box,
+                session_chat,
+                session_context],
+        outputs=[msg_box,
+                 submit_btn,
+                 session_chat,
+                 chat,
+                 session_context,
+                 status_display],
         queue=True,
         show_progress=True
     )
 
     assistant_msg_args = dict(
         fn=chatbot.assistant_msg,
-        inputs=[session_chat, session_context],
-        outputs=[session_chat, chat, session_context, status_display],
-        # show_progress=False,
+        inputs=[session_chat,
+                session_context],
+        outputs=[session_chat,
+                 chat,
+                 session_context,
+                 session_error,
+                 msg_box,
+                 submit_btn,
+                 status_display],
+        # show_progress=True,
         queue=True
     )
 
@@ -85,31 +118,43 @@ with gr.Blocks(css=customCSS, theme="sudeepshouche/minimalist") as demo:
     # Textbox enter
     user_submit = msg_box.submit(**user_msg_args)
     user_submit.then(**assistant_msg_args)
-    user_submit.then(fn=s3.upload, inputs=[session_id, session_time, session_context])
+    user_submit.then(fn=aws.s3_upload, inputs=[session_id, session_time, session_context])
+    user_submit.then(fn=serve_error, inputs=session_error)
 
     # Textbox + Send button
     submit_click = submit_btn.click(**user_msg_args)
     submit_click.then(**assistant_msg_args)
-    submit_click.then(fn=s3.upload, inputs=[session_id, session_time, session_context])
+    submit_click.then(fn=aws.s3_upload, inputs=[session_id, session_time, session_context])
+    submit_click.then(fn=serve_error, inputs=session_error)
+
+    # TODO: Finish this / decide if even needed
+    # Error info button
+    # error_info_click = error_info_btn.click(fn=gr.Info(f"Session error bool: {session_error.value}"), inputs=None,
+    #                                         outputs=None)
 
     # Clear button
-    clear_click = clear_btn.click(fn=lambda: (None, [], STATUS_MSGS['cleared']),
-                                  inputs=None,
-                                  outputs=[chat, session_chat, status_display],
+    clear_click = clear_btn.click(fn=lambda: (None, []),
+                                  outputs=[chat, session_chat],
                                   queue=False)
-    clear_click.then(fn=lambda: [{'role': 'system', 'content': SYSTEM_PROMPT}], inputs=None, outputs=session_context)
-    clear_click.then(fn=lambda: (None, []), inputs=None, outputs=[file_out, file_types])
+    clear_click.then(fn=lambda: gr.State(), outputs=session_error)
+    clear_click.then(fn=lambda: gr.update(placeholder="Type a message.", interactive=True), outputs=msg_box)
+    clear_click.then(fn=lambda: gr.update(interactive=True), outputs=submit_btn)
+    clear_click.then(fn=lambda: [{'role': 'system', 'content': SYSTEM_PROMPT}], outputs=session_context)
+    clear_click.then(fn=lambda: (None, []), outputs=[file_out, file_types])
+    clear_click.then(fn=lambda: STATUS_MSGS['cleared'], outputs=status_display)
 
     # Save Chat button
     if chat_saving:
         file_types.input(fn=lambda types: gr.update(interactive=True) if any(types) else gr.update(interactive=False),
                          inputs=[file_types],
                          outputs=[save_btn])
-        save_btn.click(fn=s3.serve_files, inputs=[session_id, session_time, file_types], outputs=file_out)
+        save_btn.click(fn=aws.serve_files, inputs=[session_id, session_time, file_types], outputs=file_out)
 
-logger.info('Launching the app...')
+logger.info("Launching the app...")
+demo.queue(concurrency_count=1)
 try:
-    demo.queue().launch(share=True)  # for local
-    # demo.queue().launch(server_name="0.0.0.0", server_port=8080, ssl_verify=False) # for aws
+    if __name__ == "__main__":
+        # demo.launch(share=True)  # for local
+        demo.launch(server_name="0.0.0.0", server_port=8080, ssl_verify=False)  # for aws
 except Exception as e:
     logger.error('An error occurred while launching the app:', exc_info=True)
