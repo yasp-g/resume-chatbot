e70a106ac83afa54e0f817c0d2d987dced9dcb42
yasp-g <jasper.h.g@icloud.com>
2023-11-16 23:13:04 +0100

    - now importing `timing_decorator` from its own module - added logging with `logger = logging.getLogger()` - added `warm_up_api()`   - This function initiates a very short completion - added `create_openai_client()`   - this function is used to create the OpenAI client - added logger INFO note to start of `user_msg()` noting the time a new message is received. - `get_chat_completion`   - added client input to   - reconfigured function to work with new OpenAI client workflow   - reconfigured exception handling to create `logger.error` events - `assistant_msg()`   - added client input   - added manual timing code with logging

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -1,32 +1,57 @@
-import os
-import time
+import logging
 import openai
+import os
 import requests
 import socket
+import time
 import urllib3
 
 import gradio as gr
 
+from openai import OpenAI
 from .config import STATUS_MSGS
+from .timing_decorator import timing_decorator
 
-import functools
-
-
-def timing_decorator(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        print(f"Function {func.__name__!r} executed in {(end_time - start_time):.4f}s")
-        return result
-
-    return wrapper
-
-# Constants
+# SETUP
+logger = logging.getLogger()
+logger.info(f"initial api key check: {bool(os.environ.get('OPENAI_API_KEY'))}")
 GPT = 'gpt-4'
 # GPT = 'gpt-3.5-turbo'
 TEMP = 0.1
+
+
+# TODO: decide if this is needed
+@timing_decorator
+def warm_up_api(client):
+    logger.info(f"warmup check: {bool(os.environ.get('OPENAI_API_KEY'))}")
+    # if not client.api_key:
+    #     client.api_key = os.environ.get('OPENAI_API_KEY')
+    # if OpenAI.api_key is None:
+    #     raise EnvironmentError("No OpenAI API key.")
+    # if not os.environ.get('OPENAI_API_KEY'):
+    #     raise EnvironmentError("No OpenAI API key.")
+
+    try:
+        client.chat.completions.create(
+            model=GPT,  # Use an appropriate model
+            messages=[{"role": "system", "content": "You are a helpful assistant."}],
+            temperature=0
+        )
+        logger.info("API warm-up call completed.")
+    except Exception as e:
+        logger.info(f"API warm-up call failed: {e}")
+
+
+@timing_decorator
+def create_openai_client():
+    # api_key = os.environ.get('OPENAI_API_KEY')
+    if os.environ.get('OPENAI_API_KEY') is None:
+        raise EnvironmentError("No OpenAI API key.")
+
+    client = OpenAI()  # api_key=api_key)
+    warm_up_api(client)
+
+    return client
 
 
 @timing_decorator
@@ -38,6 +63,7 @@ def user_msg(message, history, context):
     :param context: input from gr.State session_context component
     :return: output to gr.Textbox component, output to gr.Chatbot component, updated session_context, output to gr.Markdown component
     """
+    logger.info("---NEW MESSAGE---")
     context += [{'role': 'user', 'content': message}]
     history += [[message, None]]
     return gr.update(value="", interactive=False), gr.update(interactive=False), history, history, context, STATUS_MSGS[
@@ -45,67 +71,65 @@ def user_msg(message, history, context):
 
 
 @timing_decorator
-def get_chat_completion(messages, model=GPT, temperature=TEMP):
+def get_chat_completion(client, messages, model=GPT, temperature=TEMP):
     """Get chat completion from openai API.
 
+    :param client: OpenAI client
     :param messages: list of messages comprising the conversation so far
     :param model: ID of the model to use
     :param temperature: What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
     :return: openAI chatGPT chat completion content, error message if relevant
     """
-
-    if not openai.api_key:
-        openai.api_key = os.environ.get('OPENAI_API_KEY')
-
-    if openai.api_key is None:
-        raise EnvironmentError("No OpenAI API key.")
-
     try:
-        chat_completion = openai.ChatCompletion.create(
+        completion = client.chat.completions.create(
             model=model,
             messages=messages,
             temperature=temperature
         )
-        return chat_completion.choices[0].message["content"], None, None
+        return completion.choices[0].message.content, None, None
 
     # TODO: handle network errors better (maybe: remove message from context, remove from history when new message sent)
     except (socket.gaierror, urllib3.exceptions.NewConnectionError, urllib3.exceptions.MaxRetryError,
-            requests.exceptions.ConnectionError, openai.error.APIConnectionError) as e:
-        error_message = f"A network error occurred: {e}. Please try again."
-        print(f"Network error occurred: {type(e).__name__}: {e}")
+            requests.exceptions.ConnectionError, openai.APIConnectionError) as e:
+        error_message = f"A network error occurred: {e}. Please try again by refreshing the page."
+        logger.error(f"Network error occurred: {type(e).__name__}: {e}")
         return None, e, error_message
 
-    except openai.error.InvalidRequestError as e:
+    except openai.RateLimitError as e:
         error_message = f"""
         Congratulations, your conversation with ResumeBot has reached OpenAI's token limit! \
         Future versions of ResumeBot will work around this constraint, but for now, this is the end of your conversation. \
         Simply refresh the page to start again if you'd like. I hope you had fun and come back soon :).
         """
-        print(f"Request error occurred: {type(e).__name__}: {e}")
+        logger.error(f"Request error occurred: {type(e).__name__}: {e}")
         # print(f"error message: {error_message}")
         return None, e, error_message
 
     except Exception as e:
-        error_message = f"An unexpected error occurred: {e}. Please try again."
-        print(f"Unexpected error of type {type(e).__name__}: {e}")
+        error_message = f"An unexpected error occurred: {e}. Please try again by refreshing the page."
+        logger.error(f"Unexpected error of type {type(e).__name__}: {e}")
         return None, e, error_message
 
 
-@timing_decorator
-def assistant_msg(chat_history, chat_context):
+# @timing_decorator
+def assistant_msg(client, chat_history, chat_context):
     """Update gradio chat with openai chat completion.
 
+    :param client: OpenAI client
     :param chat_history: input from gr.Chatbot component
     :param chat_context: input from gr.State session_context component
     :return: yields chat completion, updated session_context, output to gr.Markdown component
     """
-    completion, e, error_message = get_chat_completion(chat_context)
+    start_time = time.time()  # Start timing here
+    completion, e, error_message = get_chat_completion(client, chat_context)
 
     if error_message:
         chat_context += [{'role': 'assistant', 'content': error_message}]
         chat_history[-1][1] = error_message
         msg_box_update = gr.update(value="", placeholder="Chat locked due to an error.", interactive=False)
         submit_btn_update = gr.update(interactive=False)
+        end_time = time.time()  # End timing before yielding
+        logger.info(f"Function `assistant_msg` executed in {(end_time - start_time):.4f}s with error")
         yield chat_history, chat_history, chat_context, e, msg_box_update, submit_btn_update, STATUS_MSGS['error']
 
     else:
@@ -120,4 +144,6 @@ def assistant_msg(chat_history, chat_context):
             yield chat_history, chat_history, chat_context, e, msg_box_update, submit_btn_update, STATUS_MSGS[
                 'responding']
 
+        end_time = time.time()  # End timing after the loop
+        logger.info(f"Function `assistant_msg` executed in {(end_time - start_time):.4f}s with error")
         yield chat_history, chat_history, chat_context, e, msg_box_update, submit_btn_update, STATUS_MSGS['ready']
