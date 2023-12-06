190ebcf4f7fe431d0c420ad83490bad3bfd8553f
yasp-g <jasper.h.g@icloud.com>
2023-12-06 14:41:52 +0100

    setup_gradio_app.py - new gradio components:   - `help_text`     - initially invisible `gr.Markdown()` object     - made visible by files_selected_check() (see below)   - `file_out_placeholder`     - blank `gr.Markdown()` object which "holds the space" of `file_out`     - made invisible by `files_selected_check()` (removing it from the page)     - i decided to make `file_out` invisible by default       - it looked weird sitting there blank before any files were served to it       - the `gr.Group()` containing `file_types` looked bad on desktop without `file_out`         - it would stretch to the width of the page then shrinking to half-page when files were served       - `file_out_placeholder` solves this problem - added `files_selected_check()` function   - if a `file_types` selection is made and `s_context` is empty:     - `file_types` -> cleared, `save_btn` -> remains not interactive, `help_text` -> made visible   - if a `file_types` selection is made and `s_context` is not empty:     - `file_types` -> unchanged, `save_btn` -> made interactive, `help_text` -> made invisible   - if a `file_types` selection is cleared:     - `file_types` -> unchanged, `save_btn` -> made not interactive, `help_text` -> made invisible - moved custom.css read inside of `setup_gradio_app()` function - added title to `gr.Blocks()` - specified version `@0.0.1` for theme in `gr.Blocks()`   - light mode chat-box looks better this way     - i felt the lack of border/background color looked bad in light mode - removed the tabs   - now all components load on the landing page - added `avatar_images` to `chat`   - just for the bot, no image for user   - added prompt help with `info` parameter     - after first message this is removed   - added custom css to increase font size     - see custom.css commit - added styling from custom.css to `DESCRIPTION_BOTTOM` markdown object - fixed formatting consistency of inputs/outputs in `msg_box.submit(...)` and `submit_btn.click(...)` - added `clear_btn` to needed inputs/outputs of `chatbot.user_msg` `chatbot.assistant_msg`   - see previous commit for chatbot.py - configured `clear_btn.click(...)` to align with new changes   - also now resetting `s_id` and `s_timestamp` to prevent overwriting convo log - `save_btn.click(...)` now swaps makes `file_out_placeholder` invisible and `file_out` visible before running `aws.serve_files`

--- setup_gradio_app.py
+++ setup_gradio_app.py
@@ -15,8 +15,15 @@ def serve_error(error):
         return gr.Warning(f"{error}")
 
 
-with open("static/custom.css", "r", encoding="utf-8") as f:
-    customCSS = f.read()
+@timing_decorator
+def files_selected_check(types, chat_content):
+    if any(types):
+        if not chat_content:
+            return gr.update(value=None), gr.update(interactive=False), gr.update(visible=True)
+        else:
+            return types, gr.update(interactive=True), gr.update(visible=False)
+    else:
+        return types, gr.update(interactive=False), gr.update(visible=False)
 
 
 def setup_gradio_app():
@@ -24,15 +31,18 @@ def setup_gradio_app():
     chat_saving = True
     # logging_check = True
 
-    with gr.Blocks(title="ResumeBot by Jasper", css=customCSS, theme="sudeepshouche/minimalist") as demo:
+    with open("static/custom.css", "r", encoding="utf-8") as f:
+        custom_css = f.read()
+
+    with gr.Blocks(title="ResumeBot by Jasper", css=custom_css, theme="sudeepshouche/minimalist@0.0.1") as demo:
         # SETUP
         # Session variables
         s_id = gr.State(lambda: str(uuid.uuid1()))
         s_time = gr.State(lambda: datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
         s_chat = gr.State([])
         s_client = gr.State(chatbot.create_openai_client)
-        s_context = gr.State()
-        s_error = gr.State()
+        s_context = gr.State("")
+        s_error = gr.State("")
 
         # GRADIO LAYOUT
         # Title, description, status display
@@ -42,32 +52,41 @@ def setup_gradio_app():
         status_display = gr.Markdown(STATUS_MSGS['waiting'], elem_id="status_display")
 
         # Chatbot Tab
-        with gr.Tab("Chatbot"):
-            # TODO: see whats best to do now that gr.Group() is deprecated
-            with gr.Group():
-                chat = gr.Chatbot(label="Chat")
-                with gr.Row(variant='panel', equal_height=False, elem_id='submit_button_container'):
-                    # TODO: check if container=False bug has been addressed
-                    msg_box = gr.Textbox(show_label=False, placeholder="Type a message.", scale=5, max_lines=10)
-                    submit_btn = gr.Button("Send", scale=1, variant='primary')
-            clear_btn = gr.Button("Clear Conversation", variant='stop')
-            # error_info_btn = gr.Button("Error Info")
-            # chat_saving_check = gr.Checkbox(True, label="Chat Logging", visible=logging_check, interactive=True)
+        with gr.Group():
+            chat = gr.Chatbot(label="ResumeBot", avatar_images=(None, "utils/images/favicon.ico"))
+            with gr.Row(variant='panel', equal_height=True):  # , elem_id='submit_button_container'):
+                # TODO: check if container=False bug has been addressed
+                msg_box = gr.Textbox(max_lines=10,
+                                     placeholder="Type a message.",
+                                     info="If you're feeling stuck, just try \"hello\" :)",
+                                     show_label=False,
+                                     container=True,
+                                     scale=5,
+                                     min_width=320,
+                                     elem_id='msg_box'
+                                     )
+                submit_btn = gr.Button("Send", scale=1, variant='primary', elem_id='submit_btn')
+        clear_btn = gr.Button("Clear Conversation", variant='stop')
+        # error_info_btn = gr.Button("Error Info")
+        # chat_saving_check = gr.Checkbox(True, label="Chat Logging", visible=logging_check, interactive=True)
 
         # Download Tab
         if chat_saving:
-            with gr.Tab("Download Chat"):
-                with gr.Row():
-                    with gr.Group(elem_id="download_files_container"):
-                        with gr.Column(scale=1, min_width=480):
-                            gr.Markdown("<center>Select file type(s) and click Save Chat</center>")
-                            file_types = gr.CheckboxGroup([".txt", ".json", ".csv"], show_label=False,
-                                                          elem_id="file_types")
-                            save_btn = gr.Button("Save Chat", scale=1, interactive=False, variant='primary',
-                                                 elem_id="save_files_inputs")
-                    file_out = gr.File(label="Saved File(s)", scale=2, min_width=480, interactive=False)
+            # with gr.Accordion("Download Chat", open=False):
+            with gr.Row(elem_id='custom-row'):
+                with gr.Group(elem_id='download_files_container'):
+                    # TODO: is min_width still needed here with new custom.css styling?
+                    with gr.Column(scale=1, min_width=1000):
+                        gr.Markdown("<center>Select file type(s) and click Save Chat</center>")
+                        file_types = gr.CheckboxGroup([".txt", ".json", ".csv"], show_label=False,
+                                                      elem_id="file_types")
+                        help_text = gr.Markdown("<center>Try having a convo first!</center>", visible=False)
+                        save_btn = gr.Button("Save Chat", interactive=False, variant='primary', scale=1,
+                                             elem_id='save_files_inputs')
+                file_out_placeholder = gr.Markdown("")
+                file_out = gr.File(label="Downloaded File(s)", scale=1, min_width=480, interactive=False, visible=False)
 
-        gr.Markdown(DESCRIPTION_BOTTOM)
+        gr.Markdown(DESCRIPTION_BOTTOM, elem_id="description_bottom")
         server_name = gr.Markdown(f"Server: {os.getenv('INSTANCE_NAME')}", elem_id="server_name")
 
         # APP ACTIONS
@@ -77,11 +96,12 @@ def setup_gradio_app():
             inputs=[msg_box,
                     s_chat,
                     s_context],
-            outputs=[msg_box,
-                     submit_btn,
-                     s_chat,
+            outputs=[s_chat,
                      chat,
                      s_context,
+                     msg_box,
+                     submit_btn,
+                     clear_btn,
                      status_display],
             queue=True,
             api_name="usr_msg_submit"
@@ -92,20 +112,24 @@ def setup_gradio_app():
                     s_chat,
                     s_context,
                     msg_box,
-                    submit_btn],
+                    submit_btn,
+                    clear_btn],
             outputs=[s_chat,
                      chat,
                      s_context,
                      s_error,
                      msg_box,
                      submit_btn,
+                     clear_btn,
                      status_display],
             queue=True,
             api_name="assistant_msg_submit",
             # show_progress=True
         ).then(  # upload chat to S3
             fn=aws.s3_upload,
-            inputs=[s_id, s_time, s_context],
+            inputs=[s_id,
+                    s_time,
+                    s_context],
             api_name="s3_upload_submit"
         ).then(  # serve error if needed
             fn=serve_error,
@@ -119,11 +143,12 @@ def setup_gradio_app():
             inputs=[msg_box,
                     s_chat,
                     s_context],
-            outputs=[msg_box,
-                     submit_btn,
-                     s_chat,
+            outputs=[s_chat,
                      chat,
                      s_context,
+                     msg_box,
+                     submit_btn,
+                     clear_btn,
                      status_display],
             queue=True,
             api_name="usr_msg_click"
@@ -134,20 +159,24 @@ def setup_gradio_app():
                     s_chat,
                     s_context,
                     msg_box,
-                    submit_btn],
+                    submit_btn,
+                    clear_btn],
             outputs=[s_chat,
                      chat,
                      s_context,
                      s_error,
                      msg_box,
                      submit_btn,
+                     clear_btn,
                      status_display],
             queue=True,
             api_name="assistant_msg_click",
             # show_progress=True
         ).then(  # upload chat to S3
             fn=aws.s3_upload,
-            inputs=[s_id, s_time, s_context],
+            inputs=[s_id,
+                    s_time,
+                    s_context],
             api_name="s3_upload_click"
         ).then(  # serve error if needed
             fn=serve_error,
@@ -164,14 +193,16 @@ def setup_gradio_app():
         clear_btn.click(
             fn=lambda: (None, []),
             outputs=[chat, s_chat],
-            queue=False,
+            queue=True,
             api_name="clear_chat"
         ).then(
-            fn=lambda: gr.State(),
+            fn=lambda: "",
             outputs=s_error,
             api_name="clear_error"
         ).then(
-            fn=lambda: gr.update(placeholder="Type a message.", interactive=True),
+            fn=lambda: gr.update(placeholder="Type a message.",
+                                 interactive=True,
+                                 info="If you're feeling stuck, just try \"hello\" :)."),
             outputs=msg_box,
             api_name="reset_msg_box"
         ).then(
@@ -179,13 +210,21 @@ def setup_gradio_app():
             outputs=submit_btn,
             api_name="reset_submit_btn"
         ).then(
-            fn=lambda: [{'role': 'system', 'content': s_context}],
+            fn=lambda: "",
             outputs=s_context,
             api_name="reset_context"
         ).then(
             fn=lambda: (None, []),
             outputs=[file_out, file_types],
             api_name="reset_files"
+        ).then(
+            fn=lambda: str(uuid.uuid1()),
+            outputs=s_id,
+            api_name="reset_uid"
+        ).then(
+            fn=lambda: datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
+            outputs=s_time,
+            api_name="reset_timestamp"
         ).then(
             fn=lambda: STATUS_MSGS['cleared'],
             outputs=status_display,
@@ -195,9 +234,25 @@ def setup_gradio_app():
         # Save Chat button
         if chat_saving:
             file_types.input(
-                fn=lambda types: gr.update(interactive=True) if any(types) else gr.update(interactive=False),
-                inputs=[file_types],
-                outputs=[save_btn])
-            save_btn.click(fn=aws.serve_files, inputs=[s_id, s_time, file_types], outputs=file_out)
+                fn=files_selected_check,
+                inputs=[file_types, s_chat],
+                outputs=[file_types, save_btn, help_text],
+                show_progress="hidden"
+            )
+            save_btn.click(
+                fn=lambda: gr.update(visible=False),
+                outputs=file_out_placeholder,
+                api_name="hide_file_out_placeholder"
+            ).then(
+                fn=lambda: gr.update(visible=True),
+                outputs=file_out,
+                show_progress="hidden",
+                api_name="show_file_out"
+            ).then(
+                fn=aws.serve_files,
+                inputs=[s_id, s_time, file_types],
+                outputs=file_out,
+                queue=True
+            )
 
     return demo
